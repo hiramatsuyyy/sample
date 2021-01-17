f5aed210b106475f2450779f5306859b56589cd9
uya <>
2021-01-18 00:31:55 +0900

    new project

---
+++ sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by uya on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("test")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
